62451e046ca2d39c379f01fc00407f4737c06993
AlexLecq <alexandreleclercq1@outlook.fr>
2021-04-08 09:50:39 +0200

    add menu

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-menu class="menu">
+    <h2 class="title is-2">The Right Price</h2>
+    <slot></slot>
+  </b-menu>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.menu{
+    width: 20%;
+    margin-right: 5em;
+    padding: 1em 2em 0 2em;
+    height: 100vh;
+    box-shadow: 6px 0px 23px rgb(50,50,50, .5);
+}
+</style>
